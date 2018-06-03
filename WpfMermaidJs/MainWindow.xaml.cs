41056b85dd26c0b4207f0724c28541ea748fc331
youmts <anata0814@gmail.com>
2018-06-03 16:04:33 +0900

    For local html

--- WpfMermaidJs/MainWindow.xaml.cs
+++ WpfMermaidJs/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,13 @@ namespace WpfMermaidJs
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            WebView.Url = $"file:{Directory.GetCurrentDirectory()}/mermaid.html";
         }
+
+        public string Url { get; set; }
 
         private void WebView_OnLoadFailed(object sender, LoadFailedEventArgs e)
         {
